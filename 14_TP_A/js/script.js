66651db58111975574c078e4c49d5e6675a2f137
afanasyevadina <dinadino20@gmail.com>
2019-12-09 10:28:10 +0600

    Hiding modal and comments

--- 14_TP_A/js/script.js
+++ 14_TP_A/js/script.js
@@ -7,16 +7,13 @@ var app = new Vue({
 			show: false
 		},
 		easingFunctions: [],
-		t: 0,
-		max: 0,
-		w: 0, 
+		t: 0, //current time value
+		w: 0, //chart size
 		h: 0,
-		showLink: false
+		showLink: false //back-to-top button
 	},
 	computed: {
-		scrolled: function() {
-			return window.scrollY > 0
-		},
+		//style for slider
 		progressStyle: function() {
 			return {
 				background: 'linear-gradient(90deg, #4c5f94 ' + this.t +'%, #c0c8dc ' + this.t + '%)'
@@ -24,6 +21,7 @@ var app = new Vue({
 		},
 	},
 	methods: {
+		//calculates transition function
 		fun: function(t, formula) {
 			if(this.current === null) return 0
 			t = t/100
@@ -32,15 +30,17 @@ var app = new Vue({
 			let max = eval(formula)
 			return cur * 100 / max
 		},
+		//generates curve from function
 		path: function(time, formula) {
 			var path = ['M 20,' + (this.h + 20)];
 			for(let t = 0; t <= time; t += 1) {
 				let x = t*this.w/100 + 20
 				let y = (this.fun(t, formula)*this.h/-100)+this.h + 20
-				path.push('L' + x+','+y);
+				path.push('L ' + x + ',' + y);
 			}
 			return path.join(' ')
 		},
+		//increases the time value to 100
 		play: function() {
 			var vm = this
 			var inter = setInterval(function() {
@@ -49,6 +49,7 @@ var app = new Vue({
 					clearInterval(inter)
 			}, 30)
 		},
+		//checking if the back to top button must be shown
 		onScroll: function() {
 			this.showLink = window.scrollY > 50
 		},
@@ -58,17 +59,19 @@ var app = new Vue({
 		}
 	},
 	created() {
+		//load data from json file
 		fetch('json/easing-functions-subset-2.json')
 		.then(response => response.json())
 		.then(json => {			
 			app.modal.text = json.description
-			.replace(/((https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)/, '<a target="_blank" href="$1">$1</a>');
+			.replace(/((https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)/, '<a target="_blank" href="$1">$1</a>'); //replace url with hyperlink
 			app.easingFunctions = json.easingFunctions;
 		})
 		window.addEventListener('scroll', this.onScroll)
-		window.addEventListener('resize', this.calcSize)
+		window.addEventListener('resize', this.calcSize) //adaptive in testing
+		window.addEventListener('keydown', (e) => {if(e.keyCode == 27) this.modal.show = false}) //hide modal dialog by Esc
 	},
 	mounted() {
-		this.calcSize()
+		this.calcSize() //sets the w and h properties
 	}
 })
