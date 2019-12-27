84d3e1fab369ddf2a02d71ba618ced9f37e65234
afanasyevadina <dinadino20@gmail.com>
2019-12-27 17:59:27 +0600

    file upload zone

--- 14_TP_B/resources/views/admin/symbols.blade.php
+++ 14_TP_B/resources/views/admin/symbols.blade.php
@@ -3,8 +3,10 @@
 <main class="small-4 medium-3 col auto">
     <form action="symbols" method="POST" enctype="multipart/form-data">
         @csrf
-        <label>
-            <input type="file" name="symbols[]" accept="*.png" multiple required>
+        <label class="file-input">
+            <div></div>
+            <img class="bg" src="{{ asset('public/img/select.png') }}" alt="Select files">
+            <input type="file" id="file" name="symbols[]" accept="*.png" multiple required>
         </label>
         <input type="submit" value="Upload">
     </form>
@@ -24,4 +26,23 @@
         @endforeach
     </ul>
 </main>
-@endsection
+<script type="text/javascript">
+    document.getElementById('file').addEventListener('change', function(){
+        for(var i = 0; i < this.files.length; i++) {
+            if(this.files[i].type.indexOf('image') != -1) {
+                var reader = new FileReader();
+                reader.onload = (function(theFile) {
+                    return function(e) {
+                            // Render thumbnail.
+                            var span = document.createElement('span');
+                            span.innerHTML = ['<img class="thumbnail" title="', theFile.name, '" src="', e.target.result, '" />'].join('');
+                            document.querySelector('.file-input > div').insertBefore(span, null);
+                            document.querySelector('.file-input').classList.add('filled');
+                        };
+                    })(this.files[i]);
+                    reader.readAsDataURL(this.files[i]);
+                }
+            }
+        });
+    </script>
+    @endsection
